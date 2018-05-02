95eb55145d449943226f498308168bc6a0032471
BookXHD <33343442+BookXHD@users.noreply.github.com>
2018-05-02 11:27:55 +0800

    用户详情接口

--- src/main/java/com/tzg/xhd/tbooking/controller/IndexController.java
+++ src/main/java/com/tzg/xhd/tbooking/controller/IndexController.java
@@ -1,15 +1,21 @@
 package com.tzg.xhd.tbooking.controller;
 
+import com.tzg.xhd.tbooking.common.Answer;
+import com.tzg.xhd.tbooking.common.AnswerGenerator;
 import com.tzg.xhd.tbooking.entity.User;
 import com.tzg.xhd.tbooking.util.HttpSessionUtil;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Api(description = "首页")
+@Slf4j
 @Controller("indexController")
 public class IndexController {
+    @ApiOperation(value = "主页", notes = "跳转主页 攻略页面")
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public String main(Model model){
         User user = HttpSessionUtil.getLoginUserSession();
@@ -17,13 +23,26 @@ public class IndexController {
         return "index";
     }
 
+    @ApiOperation(value = "是否登录", notes = "判断用户是否登录 返回true/false")
     @RequestMapping(value = "/ifLogin",method = RequestMethod.GET)
     @ResponseBody
     public boolean ifLogin(){
         User user = HttpSessionUtil.getLoginUserSession();
-        if(null != user){
-            return true;
+        if(null != user){            return true;
         }
         return false;
+    }
+
+    @ApiOperation(value = "用户详情", notes = "返回用户个人的详情")
+    @RequestMapping(value = "/userDetail ",method = RequestMethod.POST)
+    @ResponseBody
+    public Answer getUserDetail(){
+        Answer answer = new Answer();
+        User user = HttpSessionUtil.getLoginUserSession();
+        if(null != user){
+            answer = AnswerGenerator.genFailAnswer("用户未登录");
+        }
+        answer = AnswerGenerator.genSuccessAnswer(user);
+        return answer;
     }
 }
